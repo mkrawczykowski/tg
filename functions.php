d8d57f019706c8ffecd25c484da504ee14bc43cf
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-04-23 11:12:12 +0200

    sections structure and styles structure

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
   require_once get_template_directory() . '/includes/acf-functions.php';
   require_once get_template_directory() . '/includes/styles-scripts.php';
+  require_once get_template_directory() . '/includes/gutenberg.php';
 ?>
--- header.php
+++ header.php
@@ -5,10 +5,12 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>test</title>
+  <title><?php the_title(); ?></title>
 </head>
 
 <body class="<?php echo  (is_user_logged_in()) ? "top-margin" : null; ?>">
-    <heading>
-
-    </heading>
+  <header>
+    <div class="logo"></div>
+    <div class="quick-contact"></div>
+    <nav class="main-nav"></div>
+  </header>
