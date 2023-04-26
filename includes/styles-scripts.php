2d246eb16abffc8f63fd3f05c73e0a0a24e8da3b
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-04-26 20:31:45 +0200

    testing wp_enqueue_scripts

--- includes/styles-scripts.php
+++ includes/styles-scripts.php
@@ -1,4 +1,15 @@
 <?php
-  wp_enqueue_style('main-style', get_stylesheet_directory_uri() . '/dist/style.min.css', array());
-  wp_enqueue_script('theme-scripts', get_template_directory_uri() . '/dist/scripts.min.js', array('jquery'), '1.0', true);
+  // function enqueue_style_scripts() {
+  //   wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/dist/style.min.css', array());
+  //   // wp_enqueue_style('main-style', get_template_directory_uri() . '/dist/style.min.css', array(), false, true);
+  //   // wp_enqueue_script('theme-scripts', get_template_directory_uri() . '/dist/scripts.min.js', array(), false, true);
+  // }
+
+  // add_action( 'wp_enqueue_scripts', 'enqueue_style_scripts' );
+
+  function my_scripts(){
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/dist/style.min.css', array());
+
+  }
+  add_action('wp_enqueue_scripts', 'my_scripts');
 ?>
