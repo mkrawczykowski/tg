444d494fb9309c8ab5d59292e7f71bbbdcde0748
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-04-26 13:17:49 +0200

    display_column_content

--- sections/flexible/heading_columns.php
+++ sections/flexible/heading_columns.php
@@ -1,8 +1,23 @@
 <?php
   global $post;
-  $heading_one = get_field('heading_one');
-  $image_or_text_right = get_field('image_or_text_right');
-  $image_or_text_left = get_field('image_or_text_left');
+  $heading_one = get_sub_field('heading_one', $post->ID);
+  $image_or_text_right = get_sub_field('image_or_text_right', $post->ID);
+  $image_or_text_left = get_sub_field('image_or_text_left', $post->ID);
+  
+  function display_column_content($side_string, $image_or_text){
+    if (isset($side_string) && $image_or_text !== 'wybierz'){
+      if ($image_or_text === 'obraz'){
+        $field_name = 'image_' . $side_string;
+        echo get_sub_field($field_name, $post->ID);
+      }
+      if ($image_or_text === 'tekst'){
+        $lead_field_name = 'lead_' . $side_string;
+        $text_field_name = 'text_' . $side_string;
+        echo get_sub_field($lead_field_name, $post->ID);
+        echo get_sub_field($text_field_name, $post->ID);
+      }
+    }
+  }
 ?>
 
 <section class="heading-columns" <?php display_margins_for_page($post->ID) ?>>
@@ -10,11 +25,11 @@
     <div class="row">
 
       <div class="col col--first">
+        <?php display_column_content('left', $image_or_text_left); ?>
       </div>
       <div class="col col--second">
-testy
+        <?php display_column_content('right', $image_or_text_left); ?>
       </div>
     </div>
   </div>
-
 </setion>
